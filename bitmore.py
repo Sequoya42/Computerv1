320fa2634761223ac46521fdec38db99747a3f11
Raphael BAUM <rbaum@e1r10p14.42.fr>
2015-09-08 18:09:38 +0200

    regular commit

--- bitmore.py
+++ bitmore.py
@@ -75,13 +75,15 @@ def alpha_bet(str):
 	a = b = c = t = tt = 0
 	for n in reversed(range(0, m)):
 		for i in lst:
-			if i[1] == 2:
+			if i[1] == n == 2:
 				a += i[0]
-			if i[1] == 1:
+				print a, i[0]
+			elif i[1] == n == 1:
 				b += i[0]
-			if i[1] == 0:
+				print b, i[0], n, i[1], i
+			elif i[1] == n == 0:
 				c += i[0]
-			if i[1] == n:
+			elif i[1] == n:
 				t += i[0]
 		if t != 0:
 			if n > 2:
@@ -94,11 +96,18 @@ def alpha_bet(str):
 
 def solve_one(b, c):
 	r = (- c) / b
-	print "ONE SOLUTION"
+	print "Polynomial degree one. Unique solution"
 	print bclr.GREEN + "Reduced form:" ,
 	print bclr.WARNING + "\t\t %0.2f * X^1 (+) %0.2f = 0" % (b, c) + bclr.ENDC
+	print bclr.BLUE + "\t\t\t %dX + %d = 0" % (b, c) + bclr.ENDC
 	print "result for x is : %0.3f" % r
 
+
+# -----------------------------------------#
+def base_form(a,b,c):
+	print bclr.GREEN + "\nEquation sous la forme de:" ,
+	print bclr.WARNING + "\t\t %0.2f * X^2 + %0.2f * X^1 + %0.2f = 0" % (a, b, c) + bclr.ENDC
+	print bclr.BLUE + "\t\t\t\t\t %dX2 + %dX + %d = 0" % (a, b, c) + bclr.ENDC
 
 # -----------------------------------------#
 
@@ -107,35 +116,36 @@ def solve_none(c):
 
 # -----------------------------------------#
 
-def get_degree(lst):
-	x = 0
-	return x
-# -----------------------------------------#
-
 def complex_solution(a, b, c, d):
 	both = -b / (2 * a)
 	print bclr.GREEN +  "\n Negative delta, complex solution (i where sqrt(i) = -1)" +  bclr.ENDC
 	x1 =  sqrt(d) / (2 * a)
 	print "\nx1 is %0.2f + i * %.2f" % (both , x1)
 	print "x2 is %0.2f - i * %.2f" % (both , x1)
-	print "x1 can also be written as : \t ( %.2f +	 (i * %.2f)) / %.2f" % (-b, sqrt(d), 2*a)
+	print "x1 can also be written as : \t ( %.2f + (i * %.2f)) / %.2f" % (-b, sqrt(d), 2*a)
 	print "x2 can also be written as : \t ( %.2f - (i * %.2f)) / %.2f" % (-b, sqrt(d), 2*a)
 
 
 # -----------------------------------------#
 
+def solve_two(b, d, a):
+	x1 = ((- b) + sqrt(d)) / (2 * a)
+	x2 = ((-b) - sqrt(d)) / (2 * a)
+	print "x1 is :\t %f" % (x1)
+	print "x2 is :\t %f" % (x2)
+
+# -----------------------------------------#
+
 def solve(str):
 	a,b,c, lst = alpha_bet(str)
-	if a == 0 and b == 0 and c == 0:
+	if a == b == c == 0:
 		print "cannot solve polynomial degree" ,  lst
 		return 
-	if b == 0 and a == 0:
+	if b == a == 0:
 		return solve_none(c)
 	elif a == 0:
 		return solve_one(b, c)
-	print bclr.GREEN + "Equation sous la forme de:" ,
-	print bclr.WARNING + "\t\t %0.2f * X^2 + %0.2f * X^1 + %0.2f = 0" % (a, b, c) + bclr.ENDC
-	print bclr.BLUE + "\t\t\t\t\t %dX2 + %dX + %d = 0" % (a, b, c) + bclr.ENDC
+	base_form(a,b,c)
 	d = (b**2) - (4 * a * c)
 	if d < 0:
 		return complex_solution(a, b, c, d)
@@ -143,16 +153,34 @@ def solve(str):
 		r = (- b) / (2 * a)
 		print "x is %d" % (r)
 		return
-	x1 = ((- b) + sqrt(d)) / (2 * a)
-	x2 = ((-b) - sqrt(d)) / (2 * a)
-	print "x1 is :\t %f" % (x1)
-	print "x2 is :\t %f" % (x2)
-# -----------------------------------------#
+	solve_two(b,d,a)
 
+# -----------------------------------------#
+# -----------------------------------------#
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
     	solve(sys.argv[1])
     else:
-        print ("please enter your string in one argument in this form: \n\t"
+        print ("please enter your string in one argument in this format: \nexample:\t"
         + bclr.GREEN + "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0" + bclr.ENDC)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
